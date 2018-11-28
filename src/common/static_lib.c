44b140a8a87f46d6172d3ea0040e23bf0e367570
ekelen <ekelen@student.42.us.org>
2018-11-28 16:20:32 +0100

    start removing redundant ptr updates in statlib

--- src/common/static_lib.c
+++ src/common/static_lib.c
@@ -1,4 +1,5 @@
 #include <nm_otool.h>
+#include <assert.h>
 
 // Add result to ar_hdr pointer to get position of mach-O hdr
 static int		get_size(const char *name)
@@ -20,12 +21,12 @@ static char	    *get_ar_name(const char *name)
 	return (ft_strstr(name, ARFMAG) + len);
 }
 
-int add_ofile(t_file *file, void *ptr, const t_ar_hdr *header)
+int add_ofile(t_file *file, void *ptr, const void *header)
 {
     t_ofile o;
     t_mach_o *m;
 
-    o.hdr = header;
+    o.hdr = (const t_ar_hdr *)header;
     o.name_size = (size_t)get_size(o.hdr->ar_name);
     o.size = (uint32_t)ft_atoi(o.hdr->ar_size);
     o.name = (const char *)get_ar_name(o.hdr->ar_name);
@@ -36,6 +37,18 @@ int add_ofile(t_file *file, void *ptr, const t_ar_hdr *header)
         return EXIT_FAILURE;
     m->ofile = o;
     return(add_mach(&(file->mach), m));
+}
+
+static int update_archive_ptrs(t_file *file, void **ptr, t_ar_hdr **hdr, size_t *ar_size)
+{
+    if (!(*hdr = (t_ar_hdr *)ptr_check_msg(file->end, *ptr, sizeof(struct ar_hdr), "ar_hdr")))
+        return EXIT_FAILURE;
+    if (ft_atoi((*hdr)->ar_size) <= 0)
+        return (EXIT_FAILURE);
+    if (add_ofile(file, *ptr, ((void *)&hdr)) == EXIT_FAILURE)
+        return EXIT_FAILURE;
+
+    
 }
 
 int handle_archive(t_file *file)
@@ -50,23 +63,19 @@ int handle_archive(t_file *file)
     ar_size = (size_t)ft_atoi(header->ar_size);
     if (!(ptr = ptr_check_msg(file->end, (void*)header + ar_size + sizeof(struct ar_hdr), 0, "first ptr")))
         return EXIT_FAILURE;
-    if (!(header = (struct ar_hdr *)ptr_check_msg(file->end, ptr, sizeof(struct ar_hdr), "second header")))
-        return EXIT_FAILURE;
-    ptr = header;
+    // if (!(header = (struct ar_hdr *)ptr_check_msg(file->end, ptr, sizeof(struct ar_hdr), "second header")))
+    //     return EXIT_FAILURE;
+    // ptr = header;
     i = -1;
-    while (ptr) // TODO: Seems rickety
+    while (ptr < file->end) // TODO: Seems rickety
 	{
-        if (ptr == file->end) // TODO: Really? Always?
-        {
-            return EXIT_SUCCESS;
-        }
 		if (!(header = (t_ar_hdr *)ptr_check_msg(file->end, ptr, sizeof(struct ar_hdr), "ar_hdr")))
         {
         	return EXIT_FAILURE;
         }
 		if (ft_atoi(header->ar_size) <= 0)
 			return (EXIT_FAILURE);
-        if (add_ofile(file, ptr, header) == EXIT_FAILURE)
+        if (add_ofile(file, ptr, (void *)header) == EXIT_FAILURE)
             return EXIT_FAILURE;
         if (!(ptr = ptr_check_msg(file->end, ptr + ft_atoi(header->ar_size) + sizeof(struct ar_hdr), 0, "new ptr")))
             return EXIT_FAILURE;
